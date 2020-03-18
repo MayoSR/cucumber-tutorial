f190aea04e3633cabfd9c0950bdaae867dd6e15f
Mayankrao-16 <55399795+Mayankrao-16@users.noreply.github.com>
2020-03-18 10:47:03 +0530

    Added regex and steps

--- cucumber-tutorial/src/main/java/com/sapient/steps/LoginStep.java
+++ cucumber-tutorial/src/main/java/com/sapient/steps/LoginStep.java
@@ -11,14 +11,22 @@ public class LoginStep {
 		System.out.println("Application is working >>>>>>>>>>>>");
 	}
 
-	@When("^the credentials are valid$")
-	public void the_credentials_are_valid() throws Throwable {
-		System.out.println("Credentials are valid >>>>>>>>>>>>");
+	@When("^the credentials are \"([a-zA-Z0-9]{1,})\"$")
+	public void the_credentials_are_valid(String validStatus) throws Throwable {
+		System.out.println("Credentials are " + validStatus +">>>>>>>>>>>>");
 	}
 
 	@Then("^take user to home page$")
 	public void take_user_to_home_page() throws Throwable {
 		System.out.println("Take user to home page >>>>>>>>>>>>");
 	}
+
+
+	@Then("^take user to login page$")
+	public void take_user_to_login_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println();
+	}
+
 
 }
--- cucumber-tutorial/src/main/resources/login.feature
+++ cucumber-tutorial/src/main/resources/login.feature
@@ -2,5 +2,10 @@ Feature: login feature
 
   Scenario: check if user is valid login
     Given the application is working
-    When the credentials are valid
+    When the credentials are "valid"
     Then take user to home page
+
+  Scenario: check if user is invalid login
+    Given the application is working
+    When the credentials are "invalid"
+    Then take user to login page
